914aa7fe1066e78f39ec7af6fd6cb12bdb1c4005
Daniel Moniz <daniel.moniz@gmail.com>
2017-03-24 21:07:03 -0400

    First stab at some basic JS to Ruby conversion.

---
+++ README.md
@@ -0,0 +1,23 @@
+
+* variables
+* data types
+  * numbers
+    * basic math
+  * strings
+    * string interpolation
+  * symbols
+    * Not in JS!
+  * arrays
+  * hashes
+
+* control flow
+  * conditionals
+  * iteration
+  * basic looping (eg. `10.times`)
+*
+*
+*
+*
+*
+*
+*
---
+++ exercises.js
@@ -0,0 +1,87 @@
+
+var apples = 14;
+console.log(apples);
+
+console.log("I have " + apples + " apples.");
+
+
+
+
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+  'elephant': "The world's largest land mammal.",
+  school: 'A place of learning.', // no quotes needed
+  'ice cream': 'A delicious milk-based dessert.', // need quotes with spaces
+};
+
+
+
+
+var num = 16;
+if (num > 10) {
+  console.log(num + " is greater than 10.");
+} else if (num == 10) {
+  console.log(num + " is exactly 10.");
+} else {
+  console.log(num + " must be less than 10.");
+}
+
+
+
+
+for (var i=0; i < 10; i++) {
+  console.log("Doing the same thing over and over.");
+}
+
+var base = 5;
+for (var num=0; num < 20; num++) {
+  console.log(num + base);
+}
+
+var total = 0;
+for (var num=0; num < 100; num++) {
+  total += num;
+}
+console.log(total);
+
+for (var height=3; height < 16; height++) {
+  if (height > 9) {
+    console.log("You can get on the rollercoaster!");
+  } else {
+    console.log("You are too short to ride this rollercoaster.");
+  }
+}
+
+
+
+
+var containers = ['purse', 'wallet', 'backback'];
+for (var i=0; i < containers.length; i++) {
+  console.log(containers[i]);
+}
+
+// OR
+containers.forEach(function(container) {
+  console.log(container);
+});
+
+
+
+
+// function declaration
+function helloWorld() {
+  console.log('Hello world!');
+}
+// OR, function expression
+var helloWorld = function() {
+  console.log('Hello world!');
+};
+
+helloWorld();
+
+
+
+
+function add(firstNum, secondNum) {
+  return first_num + second_num;
+}
